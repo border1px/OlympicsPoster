7e91cd3a8e05a0a1e5426275590e501fb69b95e4
Pengfei.Yu <border1px@163.com>
2024-08-26 16:55:16 +0800

    first commit

---
+++ README.md
@@ -0,0 +1,25 @@
+OlympicsPoster
+
+# What?
+PAI ArtLab（阿里）的奥运海报工作流复刻
+
+# Demo
+![Demo]
+
+# Introduction
+🎨 为了向在赛场上的奥运健儿传递最真挚的鼓舞与喝彩，我们特此发起一场别开生面的“创意海报设计挑战”！
+🌟 在ComfyUI工具中，选择你心中的奥运项目，或是捕捉那些令人激动的比赛瞬间，用色彩斑斓的图案、激昂的文字，定制一张专属于你的奥运会加油海报！让我们用设计的语言，汇聚成能量，为远在巴黎的运动员们加油打气，告诉他们：世界的每一个角落，都有人在为你们的每一次飞跃、每一分坚持而骄傲！
+🎯 发挥你的无限创意，生成独一无二的加油海报。完成后，更可分享至社交媒体，让这份热情感染每一个人，共同构建一场跨越国界的线上助威盛宴！
+
+[详细介绍](https://mp.weixin.qq.com/s/y3Sk5PtVT5g8yFTMJASdFw)
+
+# Params
+- sports: 运动
+- sex: 人物
+- signature：签名 
+
+# Inpaint Model
+https://huggingface.co/Acly/MAT
+
+# LoRA
+https://liblibai-online.vibrou.com/train/models/d7dd9ea7dbcc45759ba4f06b8de7d0bb/20240223-1708631710114-0020.safetensors?auth_key=1724598647-99840e87797b4436bbf3b02b875db3c2-0-c50d443dccbade129dd2328770a18d59&attname=sdxl-niji%20style_v1.0.safetensors
---
+++ olympics_poster.py
@@ -0,0 +1,134 @@
+import json
+import os
+import torch
+import numpy as np
+from PIL import Image, ImageOps
+
+def read_json_file(file_path):
+    """
+    Reads a JSON file's content and returns it.
+    Ensures content matches the expected format.
+    """
+    if not os.access(file_path, os.R_OK):
+        print(f"Warning: No read permissions for file {file_path}")
+        return None
+
+    try:
+        with open(file_path, 'r', encoding='utf-8') as file:
+            content = json.load(file)
+            # Check if the content matches the expected format.
+            if not all(['name' in item and 'prompt' in item and 'negative_prompt' in item for item in content]):
+                print(f"Warning: Invalid content in file {file_path}")
+                return None
+            return content
+    except Exception as e:
+        print(f"An error occurred while reading {file_path}: {str(e)}")
+        return None
+    
+def read_sdxl_styles(json_data):
+    """
+    Returns style names from the provided JSON data.
+    """
+    if not isinstance(json_data, list):
+        print("Error: input data must be a list")
+        return []
+
+    return [item['name'] for item in json_data if isinstance(item, dict) and 'name' in item]
+
+def find_template_by_name(json_data, template_name):
+    """
+    Returns a template from the JSON data by name or None if not found.
+    """
+    for template in json_data:
+        if template['name'] == template_name:
+            return template
+    return None
+
+def replace_prompts_in_template(template, positive_prompt, negative_prompt):
+    """
+    Replace the placeholders in a given template with the provided prompts.
+    
+    Args:
+    - template (dict): The template containing prompt placeholders.
+    - positive_prompt (str): The positive prompt to replace '{prompt}' in the template.
+    - negative_prompt (str): The negative prompt to be combined with any existing negative prompt in the template.
+ 
+    Returns:
+    - tuple: A tuple containing the replaced positive and negative prompts.
+    """
+    positive_result = template['prompt'].replace('{prompt}', positive_prompt)
+
+    json_negative_prompt = template.get('negative_prompt', "")
+    negative_result = f"{json_negative_prompt}, {negative_prompt}" if json_negative_prompt and negative_prompt else json_negative_prompt or negative_prompt
+
+    return positive_result, negative_result
+
+def load_image(filename: str):
+    folder_path = os.path.join(os.path.dirname(os.path.realpath(__file__)), "images")
+    
+    if not os.path.isdir(folder_path):
+        raise FileNotFoundError(f"Directory '{folder_path}' does not exist.")
+    
+    file_path = os.path.join(folder_path, filename)
+    
+    if not os.path.exists(file_path):
+        raise FileNotFoundError(f"File '{filename}' does not exist in the '{folder_path}' directory.")
+
+    i = Image.open(file_path)
+    i = ImageOps.exif_transpose(i)
+    img = i.convert("RGBA" if "A" in i.getbands() else "RGB")
+    img = np.array(img, dtype=np.float32) / 255.0
+    img = torch.from_numpy(img)[None,]
+    if 'A' in i.getbands():
+        mask = np.array(i.getchannel('A')).astype(np.float32) / 255.0
+        mask = 1. - torch.from_numpy(mask)
+    else:
+        mask = torch.zeros((64,64), dtype=torch.float32, device="cpu")
+    return (img, mask)
+    
+class OlympicsPoster:
+
+    sex_dict = { '男': 'man', '女': 'female', '男孩': 'boy', '女孩': 'girl' }
+
+    def __init__(self):
+        pass
+
+    @classmethod
+    def INPUT_TYPES(self):
+        self.prompts = read_json_file(os.path.join(os.path.dirname(os.path.realpath(__file__)), 'prompts.json'))
+        
+        return {
+            "required": {
+                "sports": ([item['name'] for item in self.prompts],),
+                "sex": (list(self.sex_dict.keys()),),
+                "signature": ("STRING", { "default": "Rainbow" })
+            },
+        }
+
+    RETURN_TYPES = ('STRING', 'IMAGE', 'STRING')
+    RETURN_NAMES = ('prompt', 'bg_image', 'signature')
+    FUNCTION = 'prompt_styler'
+    CATEGORY = 'utils'
+
+    def prompt_styler(self, sports, sex, signature):
+        # Process and combine prompts in templates
+        # The function replaces the positive prompt placeholder in the template,
+        # and combines the negative prompt with the template's negative prompt, if they exist.
+        template = find_template_by_name(self.prompts, sports)
+        text_positive_styled, text_negative_styled = replace_prompts_in_template(template, self.sex_dict[sex], '')
+
+        background_img, mask = load_image(f"{sports}.png")
+
+        return text_positive_styled, background_img, signature
+    
+    @classmethod
+    def IS_CHANGED(self, sports, sex, signature):
+        return f"{sports}_{sex}_{signature}"
+    
+NODE_CLASS_MAPPINGS = {
+    "OlympicsPoster": OlympicsPoster,
+}
+
+NODE_DISPLAY_NAME_MAPPINGS = {
+    "OlympicsPoster": "OlympicsPoster（PAI Artlab）",
+}
